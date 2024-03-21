ca74c31316065157ea356014438a0facb2d2e978
Julia <jules.rechi@gmail.com>
2024-03-21 20:44:54 +0100

    Add logic to redirect to login if no user token available

--- app/(tabs)/ProfileScreen.jsx
+++ app/(tabs)/ProfileScreen.jsx
@@ -1,4 +1,9 @@
-import { useFocusEffect, useNavigation } from 'expo-router';
+import {
+  Redirect,
+  useFocusEffect,
+  useNavigation,
+  useRouter,
+} from 'expo-router';
 // import { router } from 'expo-router';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
@@ -38,67 +43,71 @@ const styles = StyleSheet.create({
   },
 });
 
+//  LOG  fetched profile data: {"error": "user not found"}
+//  LOG  set user: {"error": "user not found"}
+//  LOG  set user: {"error": "user not found"}
+
 export default function ProfileScreen() {
   const profilePic = require('../../assets/profilepic.png');
+  const router = useRouter();
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const apiFetch = async () => {
       const response = await fetch(`/api/profile`).catch(console.error);
-      const user = await response.json();
-      console.log('fetched profile data:', user);
-      if (user) {
-        setUser(user);
-      }
-      if (!user) {
-        setUser(null);
-        router.push('/login');
+      const userData = await response.json();
+      console.log('fetched profile data:', userData);
+      if (userData) {
+        setUser(userData);
       }
     };
     apiFetch().catch(console.error);
   }, []);
   console.log('set user:', user);
 
+  // if (user !== null && user.error)
+  // if (user && user.error)
+  if (user?.error) {
+    return <Redirect href="/login" />;
+  }
+
+  // loading screen
+  if (!user) {
+    return (
+      <View>
+        <Text>Still loading</Text>
+      </View>
+    );
+  }
+
   const logout = async () => {
     const response = await fetch(`/api/logout`).catch(console.error);
     const data = await response.json();
     if (data.success) {
       setUser(null);
-      router.push('/login');
+      router.replace('/login');
     }
   };
 
-  if (user) {
-    return (
-      <View style={styles.container}>
-        <Image source={profilePic} />
-        <Text>
-          {user.user.firstname} {user.user.lastname}
-        </Text>
-        <Pressable
-          accessibilityLabel="logout here"
-          onPress={logout}
-          activateOpacity={0.3}
-          style={({ pressed }) => [
-            styles.button,
-            {
-              backgroundColor: pressed ? '#fff' : colors.primaryColor,
-            },
-          ]}
-        >
-          <Text style={styles.buttonText}>Logout</Text>
-        </Pressable>
-      </View>
-    );
-  } else {
-    <View>
+  return (
+    <View style={styles.container}>
+      <Image source={profilePic} />
+      <Text>
+        {user.user.firstname} {user.user.lastname}
+      </Text>
       <Pressable
-        onPress={() => {
-          router.push('/login');
-        }}
+        accessibilityLabel="logout here"
+        onPress={logout}
+        activateOpacity={0.3}
+        style={({ pressed }) => [
+          styles.button,
+          {
+            backgroundColor: pressed ? '#fff' : colors.primaryColor,
+          },
+        ]}
       >
-        Login to see profile
+        <Text style={styles.buttonText}>Logout</Text>
       </Pressable>
-    </View>;
-  }
+    </View>
+  );
 }
